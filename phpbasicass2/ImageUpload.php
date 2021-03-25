d8e84abbf1ff161875d5e7778eb88c5639cbe25a
Kartik goyal <17uec061@lnmiit.ac.in>
2021-03-25 14:21:41 +0530

    Added oop concept Create Class/PhoneValidation.php using oop concept Modified and change upload.php to index.php

---
+++ phpbasicass2/ImageUpload.php
@@ -0,0 +1,21 @@
+<?php
+  class ImageUpload{
+    public $uploadOk = 1;
+    public $path = "";
+    public $val = "";
+    public $Err = "File not uploaded";
+    function __construct($tmp_path, $img_name){
+      $check = getimagesize($tmp_path);
+      $this->path = $img_name;
+      if($check !== false){
+        if(move_uploaded_file($tmp_path, $this->path)){
+          $this->val = "file uploaded";
+        }
+        $this->uploadOk = 1;
+      } else {
+        $this->Err = "File is not an image";
+        $this->uploadOk = 0;
+      }
+    }
+  }
+?>
