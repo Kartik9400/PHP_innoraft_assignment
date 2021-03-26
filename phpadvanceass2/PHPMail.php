dd866c06e8db1836019185e0f6d4073523a9eab7
Kartik goyal <17uec061@lnmiit.ac.in>
2021-03-26 13:38:02 +0530

    TRN-72: Remove Client and server debug

--- phpadvanceass2/PHPMail.php
+++ phpadvanceass2/PHPMail.php
@@ -11,7 +11,7 @@ class PHPMail{
   function __construct() {
     $this->mail = new PHPMailer(true);
     try{
-      $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+      $this->mail->SMTPDebug = 0;                      //Enable verbose debug output
       $this->mail->isSMTP();                                            //Send using SMTP
       $this->mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
       $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
