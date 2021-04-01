fb88414bf942de46021d50f01a9717f879caf043
Kartik goyal <17uec061@lnmiit.ac.in>
2021-04-01 19:09:41 +0530

    TRN-80: Rock-paper-scissor Added comments

--- rock-paper-scissor/ComputerRPS.php
+++ rock-paper-scissor/ComputerRPS.php
@@ -1,35 +1,64 @@
 <?php
-  class ComputerRPS{
+/**
+ *
+ * @file contains class ComputerRPS Class
+ */
+/**
+ * Class ComputerRPS - find computer move and compare user & computer
+ */
+class ComputerRPS
+{
     public $computer;
     public $arr = array();
-    function __construct(){
-      $tmpValue = rand(1,3);
-      if ($tmpValue==1) {
-        $this->computer = 'r';
-      } elseif ($tmpValue==2) {
-        $this->computer = 'p';
-      } elseif ($tmpValue==3) {
-        $this->computer = 's';
-      }
-      $this->arr["pr"] = $this->arr["rs"] = $this->arr["sp"] = 2;
-      $this->arr["pp"] = $this->arr["rr"] = $this->arr["ss"] = 1;
-      $this->arr["rp"] = $this->arr["sr"] = $this->arr["ps"] = 0;
+
+    /**
+     * [__construct compute computer move and save compare array "arr"]
+     */
+    function __construct()
+    {
+        $tmpValue = rand(1, 3);
+        if ($tmpValue==1) {
+            $this->computer = 'r';
+        } elseif ($tmpValue==2) {
+            $this->computer = 'p';
+        } elseif ($tmpValue==3) {
+            $this->computer = 's';
+        }
+        $this->arr["pr"] = $this->arr["rs"] = $this->arr["sp"] = 2;
+        $this->arr["pp"] = $this->arr["rr"] = $this->arr["ss"] = 1;
+        $this->arr["rp"] = $this->arr["sr"] = $this->arr["ps"] = 0;
 
     }
 
-    function validate($content){
-      if($content == 'r' or $content == 'p' or $content == 's'){
-        return 1;
-      } else {
-        return 0;
-      }
+    /**
+     * [validate description]
+     *
+     * @param [string] $content [input from user]
+     *
+     * @return [int]          [check whether the content is right]
+     */
+    function validate($content)
+    {
+        if ($content == 'r' or $content == 'p' or $content == 's') {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
-    function check($RPS){
-      $index = $RPS.$this->computer;
-      echo ($this->arr[$index]==2 ? "user wins" :
-        ($this->arr[$index]==1 ? "Draw" :
-         "Computer wins"));
+    /**
+     * [check description]
+     *
+     * @param [string] $RPS [user input]
+     *
+     * @return [null]      []
+     */
+    function check($RPS)
+    {
+        $index = $RPS.$this->computer;
+        echo ($this->arr[$index]==2 ? "user wins" :
+          ($this->arr[$index]==1 ? "Draw" :
+           "Computer wins"));
     }
-  }
- ?>
+}
+?>
--- rock-paper-scissor/index.php
+++ rock-paper-scissor/index.php
@@ -1,5 +1,8 @@
 <?php
-  include "ComputerRPS.php";
+  /**
+   * @file [Attached the required class]
+   */
+  require "ComputerRPS.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,21 +10,23 @@
   <title></title>
 </head>
 <body>
+  <!-- input from the user -->
   <form method = "post" action = "<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>">
     Input{r/p/s}: <input type="textbox" name="RPS" />
     <input type="submit" name="submit">
   </form>
 
+  <!-- php process -->
   <?php
 
-    if(isset($_POST["submit"])){
-      $RPS = new ComputerRPS();
-      if(isset($RPS) and $RPS->validate($_POST["RPS"])){
-        $RPS->check($_POST["RPS"]);
-      } else {
-        echo "entered wrong value";
-      }
+    if (isset($_POST["submit"])) {
+        $RPS = new ComputerRPS();
+        if (isset($RPS) and $RPS->validate($_POST["RPS"])) {
+            $RPS->check($_POST["RPS"]);
+        } else {
+            echo "entered wrong value";
+        }
     }
-  ?>
+    ?>
 </body>
 </html>
